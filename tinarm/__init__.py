55905fee6d07c6bf4ded1392497101d31172050e
Chris Wallis <chris@inybble.com>
2023-09-29 15:19:04 +0100

    Expose some other classeson tinarm.

--- tinarm/__init__.py
+++ tinarm/__init__.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import os
-
-from tinarm.worker import StandardWorker
+from tinarm.worker import StandardWorker, DefaultIdLogFilter, HostnameFilter
 
 __title__ = "TINARM - Node creation tool for TAE workers"
 __version__ = "0.1.0"
 __author__ = "Martin West, Chris Wallis"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2023 Tin Arm Engineering Ltd."
-
 
