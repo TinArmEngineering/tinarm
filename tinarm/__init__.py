18ea02c744b2d2bce33a16e710dfb028dc39d09b
Chris Wallis <chris@inybble.com>
2023-11-30 17:15:08 +0000

    Expose tae data model in module

--- tinarm/__init__.py
+++ tinarm/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from tinarm.worker import StandardWorker, DefaultIdLogFilter, HostnameFilter
-from tinarm.api import Api
+from tinarm.api import Api, NameQuantityPair, Quantity, Unit
 
 __title__ = "TINARM - Node creation tool for TAE workers"
 __version__ = "0.1"
 __author__ = "Martin West, Chris Wallis"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2023 Tin Arm Engineering Ltd."
-
