862a458a89790aa79b6d25b251402fd39be2786b
Martin West <martin@tinarmengineering.com>
2024-03-22 13:26:58 +0100

    adjusted operating point

--- examples/M1/NominalPowerMaxSpeed_13Nm_3600rpm/operating_point.py
+++ examples/M1/NominalPowerMaxSpeed_13Nm_3600rpm/operating_point.py
@@ -3,7 +3,7 @@ import pint
 q = pint.UnitRegistry()
 
 operating_point = {
-    "current_density": 6.23 * q.A * q.mm**-2,
-    "current_angle": 255 * q.degrees,
+    "current_density": ((9.53**2 + 11.31**2) ** 0.5 * q.A) * 43 / (153.0 * q.mm**2),
+    "current_angle": 115 * q.degrees,
     "simulated_speed": 3600 * q.rpm,
 }
